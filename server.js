6a24a7303f6be49511c9dddb73340551e9a22cff
Abdullah Shakur <109961338+abdullahshakur149@users.noreply.github.com>
2024-02-16 05:12:44 +0500

    Changed views, changes in server js, few more work

--- server.js
+++ server.js
@@ -22,21 +22,13 @@ app.use(flash());//for flash messages
 
 // Routes
 const indexRoute = require('./routes/index');
-const performerRoute = require('./routes/performer');
-const aboutRoute = require('./routes/about');
-const programRoute = require('./routes/program');
 const venueRoute = require('./routes/venue');
-const contactRoute = require('./routes/contact');
 const registerRoute = require('./routes/register')
 
 
 
 app.use('/', indexRoute);
-app.use('/performers', performerRoute);
-app.use('/about', aboutRoute);
-app.use('/program', programRoute);
 app.use('/venue', venueRoute);
-app.use('/contact', contactRoute);
 app.use('/register', registerRoute);
 
 
