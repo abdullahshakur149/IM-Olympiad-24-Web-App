c534b4a51dafa25e4ce58c3edfadec8d5b154f8f
Abdullah Shakur <109961338+abdullahshakur149@users.noreply.github.com>
2024-03-05 00:12:16 +0500

    registeration closed.

--- routes/register.js
+++ routes/register.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const {newRegistration, uploadFiles} = require('../controllers/registration.controller')
+// const express = require('express');
+// const router = express.Router();
+// const {newRegistration, uploadFiles} = require('../controllers/registration.controller')
 
-router.get('/', (req, res, next) => {
-    res.render('registration');
-});
+// router.get('/', (req, res, next) => {
+//     res.render('registration');
+// });
 
-router.post('/new-registration', uploadFiles, newRegistration);
+// router.post('/new-registration', uploadFiles, newRegistration);
 
-module.exports = router;
+// module.exports = router;
